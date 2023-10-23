588450be374b392d22607c7a3f31c6f11b758de1
Joshua Hardwick <200560630@student.georgianc.on.ca>
2023-10-22 22:11:50 -0400

    Uploading Assets

---
+++ GD_LAB6/Assets/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10.0f;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDir = 0.0f;
+        float yDir = 0.0f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            yDir = 1.0f;
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            yDir = -1.0f;
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            xDir = -1.0f;
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            xDir = 1.0f;
+        }
+        Vector2 direction = new Vector2(xDir, yDir).normalized;
+        rb.velocity = direction * speed;
+    }
+}
---
+++ GD_LAB6/README.txt
@@ -0,0 +1,1 @@
+Created By: Joshua, Cole, Felix, Tristan
