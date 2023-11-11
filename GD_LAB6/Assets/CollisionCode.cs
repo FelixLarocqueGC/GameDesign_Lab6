bc6f8122ec2240260d89118e320fc5622c803061
Joshua Hardwick <200560630@student.georgianc.on.ca>
2023-11-11 15:38:20 -0500

    Added Dialogue Runners

--- GD_LAB6/Assets/CollisionCode.cs
+++ GD_LAB6/Assets/CollisionCode.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class CollisionCode : MonoBehaviour
 {
+    private DialogueRunner myDialogueRunner;
+
+
+    void Start()
+    {
+        myDialogueRunner = GetComponent<DialogueRunner>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("TextJosh"))
+        if (collision.CompareTag("Text") && Input.GetKey(KeyCode.E))
+
         {
-            
+            myDialogueRunner.enabled = !myDialogueRunner.enabled;
         }
     }
 }
